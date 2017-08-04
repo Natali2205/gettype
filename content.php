e19657774827938477151b9ee6141586a5bfc9f8
Natalia <Natalia.Vasilets@gmail.com>
2017-08-04 14:21:05 +0300

    +

--- content.php
+++ content.php
@@ -1,12 +1,16 @@
 <div class="col-md-9 ">
     <div class="container">
-		<?php
-			$pages = array('1', '2', '3', '4', '5', '6', '7', '8');
-			if (isset($_GET['task'])) {
-				if (in_array($_GET['task'], $pages)) {
-					require_once __DIR__ . '/Tasks/' . $_GET['task'] . '.php';
-				}
-			}
+        <?php
+        $pages = array('1', '2', '3', '4', '5', '6', '7', '8');
+        if (isset($_GET['task'])) {
+            if (in_array($_GET['task'], $pages)) {
+                require_once __DIR__ . '/Tasks/' . $_GET['task'] . '.php';
+            }
+        }
+        if (isset($_GET['log'])) {
+            require_once __DIR__ . '/login.php';
+        }
+
         ?>
     </div>
 </div>
--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php require 'header.php';
-
-if (isset($_GET['log'])) {
-    require_once __DIR__ . '/login.php';
-}
-
 require 'menu.php';
 require 'content.php';
-
 require 'footer.php';
